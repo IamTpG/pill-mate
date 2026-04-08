f311c91de896c05fcdadbc0da4c8aa415529c170
TimTmi <khoanart05@gmail.com>
2026-04-08 22:41:53 +0700

    fix: use standard compileSdk syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.pillmate"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.pillmate"
