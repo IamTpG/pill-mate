c743e853d1aafc6dd6d2d7cfad42257849ae3fc7
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2026-04-24 23:47:51 +0700

    chore: reduce the size of AppointmentHeader

--- app/src/main/java/com/example/pillmate/presentation/ui/components/AppointmentHeader.kt
+++ app/src/main/java/com/example/pillmate/presentation/ui/components/AppointmentHeader.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -19,7 +20,7 @@ import com.example.pillmate.R
 
 @Composable
 fun AppointmentHeader(completedCount: Int, totalCount: Int) {
-	Column(modifier = Modifier.padding(vertical = 24.dp)) {
+	Column(modifier = Modifier.padding(vertical = 24.dp).fillMaxWidth()) {
 		Row(verticalAlignment = Alignment.CenterVertically) {
 			Icon(
 				painter = painterResource(id = R.drawable.ic_user_profile), // Replace with your profile icon
@@ -53,21 +54,21 @@ fun AppointmentHeader(completedCount: Int, totalCount: Int) {
 				) {
 					Text(
 						text = "$completedCount/$totalCount",
-						modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
-						fontSize = 24.sp,
+						modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
+						fontSize = 16.sp,
 						fontWeight = FontWeight.Bold,
 						color = Color(0xFF1E5E52)
 					)
 				}
 				
-				Spacer(modifier = Modifier.width(12.dp))
+				//Spacer(modifier = Modifier.width(2.dp))
 				
 				
 				// Search Bar Placeholder
 				Row(
 					modifier = Modifier
 						.background(Color(0xFF1E5E52), RoundedCornerShape(6.dp))
-						.padding(horizontal = 2.dp, vertical = 6.dp),
+						,
 					verticalAlignment = Alignment.CenterVertically
 				) {
 					Button(
@@ -81,10 +82,10 @@ fun AppointmentHeader(completedCount: Int, totalCount: Int) {
 							Icons.Default.Search,
 							contentDescription = null,
 							tint = Color.White,
-							modifier = Modifier.size(30.dp)
+							modifier = Modifier.size(20.dp)
 						)
-						Spacer(modifier = Modifier.width(20.dp))
-						Text("Search", color = Color.White, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+						Spacer(modifier = Modifier.width(2.dp))
+						Text("Search", color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
 					}
 				}
 			}
